0513858db3b7d352a5afc9dd1e644da961c680ee
Witold Strzeboński <witold.strzebonski@poligralab.com>
2022-11-08 00:54:26 +0100

    lab3

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -2,34 +2,22 @@ package agh.ics.oop;
 
 public class World {
     public static void main(String[] args){
-        System.out.println("System wystartował");
-        run(convert(args));
-        System.out.println("System zakończył działanie");
-    }
-    public static void run(Direction[] directions){
-        for(Direction direction: directions){
-            switch (direction) {
-                case FORWARD -> System.out.println("Zwierzak idzie do przodu");
-                case BACKWARD -> System.out.println("Zwierzak idzie do tyłu");
-                case RIGHT -> System.out.println("Zwierzak skręca w prawo");
-                case LEFT -> System.out.println("Zwierzak skręca w lewo");
-                case UNKNOWN -> System.out.println("Nieznany kierunek");
-            }
-        }
-    }
+        Animal animal = new Animal();
+        System.out.println(animal);
+//        System.out.println(animal.getPosition());
+//        System.out.println(animal.getDirection());
+//        System.out.println(animal);
+//
+//        animal.move(MoveDirection.RIGHT);
+//        animal.move(MoveDirection.FORWARD);
+//        animal.move(MoveDirection.FORWARD);
+//        animal.move(MoveDirection.FORWARD);
+//        System.out.println(animal);
 
-    public static Direction[] convert(String[] args){
-        Direction[] directions = new Direction[args.length];
-        for(int i=0; i<args.length; i++){
-            switch (args[i]){
-                case "f" -> directions[i] = Direction.FORWARD;
-                case "b" -> directions[i] = Direction.BACKWARD;
-                case "r" -> directions[i] = Direction.RIGHT;
-                case "l" -> directions[i] = Direction.LEFT;
-                default -> directions[i] = Direction.UNKNOWN;
 
-            }
+        for(MoveDirection arg: OptionsParser.parse(args)){
+            animal.move(arg);
         }
-        return directions;
+        System.out.println(animal);
     }
 }
